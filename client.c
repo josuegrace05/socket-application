39a7573e2139bf39225ec2c184716fc0edb15c83
osboxes.org <joaoreyz@gmail.com>
2018-06-07 10:24:55 -0400

    Resolvido os problemas. Ambas aplicaç~oes realizando comunicaçao

--- client.c
+++ client.c
@@ -1,45 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 
+typedef char byte;
 
 
-int main(int argc, char const * argv[]){
-	
-	struct sockaddr_in address;
-	int sock = 0, valread;
-	struct sockaddr_in serv_addr;
-	char *hello = "Hello from client";
-	char buffer[1024] = {0};
+struct sockaddr_in clientAddress;
+struct sockaddr_in serverAddress;
 
-	if((sock = socket(AF_INET, SOCK_STREAM,0))< 0){
+int connectServer(){
+	int serverSocket = 0;
+	byte confirmConnection = 1;
+	byte receiveConnection;
+
+	if((serverSocket = socket(AF_INET, SOCK_STREAM,0))< 0){
 		printf("Socket Creation error");
 		return -1;
 	}
 
-	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(&serverAddress, '0', sizeof(serverAddress));
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(PORT);
 
-	if(inet_pton(AF_INET,"127.0.0.1",&serv_addr,&serv_addr.sin_addr)<= 0){
+	if(inet_pton(AF_INET,"127.0.0.1", &serverAddress.sin_addr)<= 0){
 		printf("\n Invalid Adrees");
 		return -1;
 	}
 
-	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+	if(connect(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
 		printf("Connection Failed");
 		return -1;
 	}
-	
-	send(sock, hello, strlen(hello),0);
+
+	//envia e recebe um byte de confirmaçao para ter certeza que a conexao esta estabelecida	
+
+	send(serverSocket, &confirmConnection,sizeof(byte),0);
 	printf("Hello message sent \n");
-	valread = read(sock,buffer,1024);
+	
+	if((recv(serverSocket,&receiveConnection,sizeof(byte),0)) < 0){
+		printf("Problema no recebimento da mensagem");
+		return(-1);
+	}
+	if(receiveConnection!= 0){
+		printf("Sem resposta do Servidor");
+		exit();
+	}
+
+	prinf("Servidor respondendo corretamente");
+	return serverSocket;
+}
+
+void closeConnection(int serverSocket){
+	close(serverSocket);
+}
+
+int main(int argc, char const * argv[]){
+	int length = 1024;
+	char *buffer = (char *) malloc(sizeof(char *) * length);
+	int serverSocket;
+	serverSocket = connectServer();
+
+
+/*	
+	send(serverSocket, buffer, strlen(buffer),0);
+	printf("Hello message sent \n");
+	
+
+	if((valread = recv(serverSocket,buffer,length,0)) < 0){
+		printf("Problema no recebimento da mensagem");
+		return(-1);
+	}
+	
+	if(buffer[0]== 'c'){
+		printf("Servidor respondendo\n");
+	}
+
+	//valread = read(socket,buffer,1024);
 	printf("%s\n", buffer);
+*/
+	closeConnection(serverSocket);
 
 	return 0;
 }
