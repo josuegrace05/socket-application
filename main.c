87c2b8e30718b473540f74989b802c30b09686a4
Daniel Sivaldi <danielferes@live.com>
2018-06-08 12:43:45 -0300

    Interface do menu atualizada

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+
+#define MENU " --------------------------------------------\n|                                            |\n|                   SPOOF                    |\n|                                            |\n|  1 - Enviar uma playlist                   |\n|  2 - Ver suas playlists                    |\n|  3 - Ver todas as músicas                  |\n|  4 - Fazer uma playlist                    |\n|  5 - Menu de operações                     |\n|  6 - Sair                                  |\n|                                            |\n -------------------------------------------\n"
+
+#define PROMPT ">> "
 
 char *read_line() {
     char *string = NULL;
@@ -11,44 +16,58 @@ char *read_line() {
         string = (char *) realloc (string, sizeof(char)*(i+1));
         string[i++] = c;
     } while (c != 10);
-    string[i-] = '\0';
+    string[i-i] = '\0';
     
     return string;
 }
 
 int main() {
-    int op, i = 0, n;
+    int op = 0, i = 0, n;
     char *string;
     struct dirent *de;
     DIR *dir;
     
-    printf(" --------------------------------------------\n|                                            |\n|                   SPOOF                    |\n|                                            |\n|  1 - Enviar uma playlist                   |\n|  2 - Ver suas playlists                    |\n|  3 - Ver todas as músicas                  |\n|  4 - Fazer uma playlist                    |\n|                                            |\n -------------------------------------------\n>> ");
-    scanf ("%d", &op);
-    switch (op) {
-        case 1:
-            printf("Digite o nome da playlist que deseja enviar\n>> ");
-            string = read_line();
-            free(string);
-            break;
-        case 2:
-            dir = opendir("./playlists");
-            
-            while ((de = readdir (dir)) != NULL)
-                printf("%s\n", de->d_name);
-            
-            closedir(dir);
-            break;
-        case 3:
-            dir = opendir("./myMusic");
-            
-            while ((de = readdir (dir)) != NULL)
-                printf("%s\n", de->d_name);
-            
-            closedir(dir);
-            break;
-        case 4:
-            break;
-    }
+    printf(MENU);
+    printf(PROMPT);
+    
+    do {
+        scanf ("%d", &op);
+        switch (op) {
+            case 1:
+                printf("Digite o nome da playlist que deseja enviar\n");
+                printf(PROMPT);
+                string = read_line();
+                printf("%s\n", string);
+                free(string);
+                break;
+            case 2:
+                dir = opendir("./playlists");
+                
+                while ((de = readdir (dir)) != NULL)
+                    printf("%s\n", de->d_name);
+                
+                closedir(dir);
+                printf(PROMPT);
+                break;
+            case 3:
+                dir = opendir("./myMusic");
+                
+                while ((de = readdir (dir)) != NULL)
+                    printf("%s\n", de->d_name);
+                
+                closedir(dir);
+                printf(PROMPT);
+                break;
+            case 4:
+                printf(PROMPT);
+                break;
+            case 5:
+                printf(MENU);
+                printf(PROMPT);
+                break;
+        }
+        
+    } while (op != 6);
     
     return 0;
 }
