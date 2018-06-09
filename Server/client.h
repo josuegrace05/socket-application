f31a5a2e83688294a0232b115d9acf17c251d6fb
Developer Machine <joekbg5@gmail.com>
2018-06-09 02:22:18 -0300

    addicionei a classe cleinte

---
+++ Server/client.h
@@ -0,0 +1,17 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QWidget>
+
+class Client : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Client(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CLIENT_H
